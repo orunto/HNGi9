05371c2a4e64318c42e079dabb154fd856ca8c95
Orunto Eniola <oruntoeniola@aol.com>
2022-10-30 23:22:27 +0100

    feat: profile section

--- stage-one/src/App.js
+++ stage-one/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section id='profile__section'>
+        <div id="profile__img"></div>
+        <span id="twitter">0runto</span>
+        <span id="slack">Orunto Eniola</span>
+      </section>
     </div>
   );
 }
